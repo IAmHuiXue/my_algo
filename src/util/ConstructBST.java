c82a0aaa4a74a4e5ae90ac1b5b90adb377c1ee15
Hui Xue <82717571+IAmHuiXue@users.noreply.github.com>
2021-09-27 01:13:20 -0400

    new util added

---
+++ src/util/ConstructBST.java
@@ -0,0 +1,21 @@
+package util;
+
+public class ConstructBST {
+
+    /** construct a BST via an ascending sorted array without null node */
+
+    public static TreeNode constructBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private static TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+}
