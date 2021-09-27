4df7157780c537f0002884c50c00bb61b58fba3a
Hui Xue <82717571+IAmHuiXue@users.noreply.github.com>
2021-09-26 20:51:11 -0400

    added an iterative solution

--- src/tree/ValidateBinarySearchTree.java
+++ src/tree/ValidateBinarySearchTree.java
@@ -2,21 +2,51 @@ package tree;
 
 import util.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /** https://leetcode.com/problems/validate-binary-search-tree/ */
 
 // this can effectively cover the edge cases where the values of all nodes are Integer.MIN_VALUE or Integer.MAX_VALUE
 
 public class ValidateBinarySearchTree {
-    public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, null, null);
+     static class Recursive {
+        public static boolean isValidBST(TreeNode root) {
+            return isValidBST(root, null, null);
+        }
+        private static boolean isValidBST(TreeNode root, Integer min, Integer max) {
+            if (root == null) {
+                return true;
+            }
+            if (min != null && root.val <= min || max != null && root.val >= max) {
+                return false;
+            }
+            return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+        }
     }
-    private boolean isValidBST(TreeNode root, Integer min, Integer max) {
-        if (root == null) {
+
+    static class Iterative {
+        public static boolean isValidBST(TreeNode root) {
+            // maintain a prev pointer to record the node previous to the cur node in the inorder sequence
+            TreeNode prev = null;
+            Deque<TreeNode> stack = new ArrayDeque<>();
+            while (root != null || !stack.isEmpty()) {
+                while (root != null) {
+                    stack.offerFirst(root);
+                    root = root.left;
+                }
+                TreeNode cur = stack.pollFirst();
+                if (prev != null && cur.val <= prev.val) {
+                    return false;
+                }
+                prev = cur;
+                root = cur.right;
+            }
             return true;
         }
-        if (min != null && root.val <= min || max != null && root.val >= max) {
-            return false;
-        }
-        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
     }
+
+
+
 }
+
