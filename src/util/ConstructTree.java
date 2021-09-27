8c7f7fde0c24363f6451f8c3b165c08f3224c407
Hui Xue <82717571+IAmHuiXue@users.noreply.github.com>
2021-09-27 01:08:58 -0400

    new utils added

---
+++ src/util/ConstructTree.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ConstructTree {
+    /** construct a tree via a preOrder list */
+    public static TreeNode constructTree(Integer[] preOrder) {
+        if (preOrder == null || preOrder.length == 0) {
+            return null;
+        }
+        // use LinkedList to store null
+        // need to parse array to Collections before putting into queue
+        Queue<Integer> queue = new LinkedList<>(Arrays.asList(preOrder));
+        return constructTree(queue);
+    }
+
+    private static TreeNode constructTree(Queue<Integer> queue) {
+        if (queue.isEmpty()) {
+            return null;
+        }
+        Integer cur = queue.poll();
+        if (cur == null) {
+            return null;
+        }
+        TreeNode root = new TreeNode(cur);
+        root.left = constructTree(queue);
+        root.right = constructTree(queue);
+        return root;
+    }
+}
---
+++ src/util/PrintTree.java
@@ -0,0 +1,36 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class PrintTree {
+    static final List<Integer> result = new ArrayList<>();
+    public static void printTree(TreeNode root) {
+        if (root != null) {
+            levelOrder(root);
+            while (result.get(result.size() - 1) == null) {
+                result.remove(result.size() - 1);
+            }
+        }
+        System.out.println(result);
+    }
+    private static void levelOrder(TreeNode root) {
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode cur = q.poll();
+            if (cur != null) {
+                result.add(cur.val);
+            } else {
+                result.add(null);
+            }
+            if (cur != null) {
+                q.offer(cur.left);
+                q.offer(cur.right);
+            }
+        }
+    }
+}
