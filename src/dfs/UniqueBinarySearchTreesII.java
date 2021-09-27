1efb855c0144ea4ab2fd1231202e7543f4cf1704
Hui Xue <82717571+IAmHuiXue@users.noreply.github.com>
2021-09-26 20:50:39 -0400

    new problems committed.

---
+++ src/dfs/UniqueBinarySearchTreesII.java
@@ -0,0 +1,38 @@
+package dfs;
+
+import util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** https://leetcode.com/problems/unique-binary-search-trees-ii/ */
+
+public class UniqueBinarySearchTreesII {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return helper(1, n);
+    }
+
+    private List<TreeNode> helper(int start, int end) {
+        List<TreeNode> answers = new ArrayList<>();
+        if (start > end) {
+            answers.add(null);
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftList = helper(start, i - 1);
+            List<TreeNode> rightList = helper(i + 1, end);
+            for (TreeNode left : leftList) {
+                for (TreeNode right : rightList) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    answers.add(root);
+                }
+            }
+        }
+        return answers;
+    }
+
+}
---
+++ src/tree/BalanceABinarySearchTree.java
@@ -0,0 +1,34 @@
+package tree;
+
+import util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** https://leetcode.com/problems/balance-a-binary-search-tree/ */
+
+public class BalanceABinarySearchTree {
+    public TreeNode balanceBST(TreeNode root) {
+        int[] array = deconstruct(root);
+        return ConvertSortedArrayToBinarySearchTree.sortedArrayToBST(array);
+    }
+
+    private int[] deconstruct(TreeNode root) {
+        List<Integer> array = new ArrayList<>();
+        inorder(root, array);
+        int[] result = new int[array.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = array.get(i);
+        }
+        return result;
+    }
+
+    private void inorder(TreeNode root, List<Integer> array) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, array);
+        array.add(root.val);
+        inorder(root.right, array);
+    }
+}
