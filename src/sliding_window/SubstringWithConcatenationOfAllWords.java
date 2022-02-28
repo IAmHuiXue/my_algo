dff154dc55ba358622c80acb5ba55704abd59dfc
Hui Xue <82717571+IAmHuiXue@users.noreply.github.com>
2022-02-28 15:11:51 -0500

    refactored

--- src/sliding_window/SubstringWithConcatenationOfAllWords.java
+++ src/sliding_window/SubstringWithConcatenationOfAllWords.java
@@ -1,4 +1,4 @@
-package string;
+package sliding_window;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,23 +15,24 @@ public class SubstringWithConcatenationOfAllWords {
 
     public List<Integer> findSubstring(String s, String[] words) {
         Map<String, Integer> counts = new HashMap<>();
-        for (final String word : words) {
+        for (String word : words) {
             counts.put(word, counts.getOrDefault(word, 0) + 1);
         }
         List<Integer> res = new ArrayList<>();
-        int n = s.length(), num = words.length, len = words[0].length();
-        for (int i = 0; i <= n - num * len; i++) {
-            Map<String, Integer> seen = new HashMap<>();
+        int num = words.length, len = words[0].length();
+        for (int i = 0; i <= s.length() - num * len; i++) {
+            // we should not directly modify counts because we need to compare with that at each iteration
+            // Therefore, we create another map to record the tmp result at each iteration
+            Map<String, Integer> seenAtCurrentIter = new HashMap<>();
             int j = 0;
             while (j < num) {
                 String word = s.substring(i + j * len, i + (j + 1) * len);
                 Integer count = counts.get(word);
-                if (count != null) {
-                    seen.put(word, seen.getOrDefault(word, 0) + 1);
-                    if (seen.get(word) > count) {
-                        break;
-                    }
-                } else {
+                if (count == null) {
+                    break;
+                }
+                seenAtCurrentIter.put(word, seenAtCurrentIter.getOrDefault(word, 0) + 1);
+                if (seenAtCurrentIter.get(word) > count) {
                     break;
                 }
                 j++;
